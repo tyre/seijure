f207cce7c92807c98638ac06f861d0064a988f00
Chris Maddox <tyre77@gmail.com>
2012-08-03 10:47:49 -0700

    tests with actual assertions, multiple property value support

--- src/seijure/core.clj
+++ src/seijure/core.clj
@@ -1,5 +1,4 @@
 (ns seijure.core)
-
 (defn as-str
   ([] "")
   ([x] (if (instance? clojure.lang.Named x)
@@ -15,13 +14,18 @@
 (defn css [style]
  (def css-out "")
   (let [tags (vec (take-while #(keyword? %) style))
-        pretties (drop-while #(keyword? %) style)]
+        properties (first (drop-while #(keyword? %) style))]
   (loop [tags tags]
-       (def css-out (str css-out (as-str (first tags)" ")))
+       (def css-out (str css-out (as-str (first tags) " ")))
       (when (seq (rest tags))
         (recur (rest tags))))
-  (for [[property values] pretties]
-      (def css-out (str css-out "{" (as-str property) ":" values ";}")))
+  (def css-out
+   (str css-out "{"
+    (as-str (first properties)) ": "
+    (clojure.string/join " " (vec (rest properties)))
+     ";}"
+    )
+   )
   )
   css-out
 )
--- test/seijure/test/core.clj
+++ test/seijure/test/core.clj
@@ -2,8 +2,11 @@
  (:use [seijure.core])
  (:use [clojure.test]))
 
-(deftest simple-link
- (is "a {color:blue;}" (css [:a [:color "blue"]])))
+(deftest simple-tag
+ (is (= "a {color: blue;}" (css [:a [:color "blue"]]))))
 
-(deftest link-with-class
- (is "a .clj {color:blue;}" (css [:a :.clj [:color "blue"]])))
+(deftest tag-with-class
+ (is (= "a .clj {color: blue;}" (css [:a :.clj [:color "blue"]]))))
+
+(deftest multiple-property-values
+ (is (= "a {padding: 1em 2em;}" (css [:a [:padding "1em" "2em"]]))))
